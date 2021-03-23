0f9ad6972cd083bf7079805f89cc645d32d10fda
sunwoong-m <77255000+sunwoong-m@users.noreply.github.com>
2021-03-24 00:06:50 +0900

    재귀함수 팩토리얼, 피보나치 수열

---
+++ JAGU_Fibonacci/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int Fibonacci(int n)
+{
+	if (n <= 1)
+		return n;
+	else
+		return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+
+int main()
+{
+	int a = 0;
+
+	cin >> a;
+
+	cout << Fibonacci(a);
+}
