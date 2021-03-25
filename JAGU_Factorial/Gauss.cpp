c0e8546d32bbdb2dea1d8085508da6693dd29176
sunwoong-m <77255000+sunwoong-m@users.noreply.github.com>
2021-03-25 12:59:52 +0900

    가우스 계산식(1 ~ n까지 모두 더하는 공식)

---
+++ JAGU_Factorial/Gauss.cpp
@@ -0,0 +1,19 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int Gauss(int n)
+//{
+//	return (((n + 1) * n) / 2);
+//}
+//
+//int main()
+//{
+//	int n = 0;
+//
+//	cin >> n;
+//
+//	cout << Gauss(n) << endl;
+//
+//	return 0;
+//}
