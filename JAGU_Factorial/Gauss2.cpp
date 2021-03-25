88a33aad6c3ff637a5236397bbe1c276d36795a2
sunwoong-m <77255000+sunwoong-m@users.noreply.github.com>
2021-03-25 13:22:12 +0900

    1 ~ n 까지 더하는 함수(가우스 공식 X)

---
+++ JAGU_Factorial/Gauss2.cpp
@@ -0,0 +1,20 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int Gauss(int n)
+//{
+//	if (n == 1)
+//		return 1;
+//	else
+//		return (n)+Gauss(n - 1);
+//}
+//
+//int main()
+//{
+//	int n = 0;
+//
+//	cin >> n;
+//
+//	cout << Gauss(n) << endl;
+//}
